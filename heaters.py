ed6af1af2d56bedd49ef5dc70770835315c17187
wahb8 <abdulwahabmoe18@gmail.com>
2025-08-09 10:06:21 +0300

    updates

--- heaters.py
+++ heaters.py
@@ -1,10 +1,17 @@
+# LEETCODE PROBLEM HEATERS (MEDIUM)
 
+# The problem we are trying to solve is the problem of finding the minimum radius a heaters can have so all the houses are heated
+# We will get two inputs
+# s = [1,2,3,4]
+# which is the locations of all the houses we have
 
+# h = [1,4]
+# which is the locations of all the heaters we have
 
-
-
-
-
+# we want to find the minimum radius a heater can have so all the houses are heated
+# with the inputs we have, the minimum r would be 1
+# since heater on location 1 can heat house 1 and 2
+# and heater 4 can heat houses on location 4 and 3
 
 def makeList(s, h):
     s.sort()
@@ -23,13 +30,16 @@ def makeList(s, h):
     arr = [0] * (maxR + 1) 
 
     for i in range(maxR + 1):
-        print(i)
-        if i in s:
+        if i in h:
+            arr[i] = f'{i}h'
+        elif i in s:
             arr[i] = f'{i}s'
-        elif i in h:
-            arr[i] = f'{i}h' 
+
 
     return arr
 
 print(makeList([1,2,5],[3]))
+print(makeList([1,2,5],[2]))
+
+def addTrees(tree1,tree2):
 
